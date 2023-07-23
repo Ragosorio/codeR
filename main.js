f977e31a703f51ac4d8c99e01e320f58e4a72787
Rolando Osorio <ragosorio777@gmail.com>
2023-07-23 16:49:18 -0600

    Add support for monaco editor

--- main.js
+++ main.js
@@ -2,8 +2,28 @@ import "./style.css";
 import Split from "split-grid";
 import r from "./r"
 import { encode, decode } from 'js-base64'
+import * as monaco from 'monaco-editor';
+import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import JsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
-function init() {
+const $js = r("#js");
+const $html = r("#html");
+const $css = r("#css");
+
+const COMMOND_EDITOR_OPTIONS = {
+  automaticLayout: true,
+  fontSize: 14,
+  theme: "vs-dark"
+}
+
+window.MonacoEnvironment = {
+  getWorker (_, label){
+    if (label === "html") return new HtmlWorker()
+    if (label === "javascript") return new JsWorker()
+    if (label === "css") return new CssWorker()
+  }
+}
   const { pathname } = window.location
   const [rawHtml, rawCss, rawJs] = pathname.slice(1).split('%7C')
 
@@ -11,13 +31,30 @@ function init() {
   const css = decode(rawCss)
   const js = decode(rawJs)
 
-  $html.value = html
-  $css.value = css
-  $js.value = js
+  const htmlEditor = monaco.editor.create($html, {
+    value: html,
+    language: 'html',
+    ...COMMOND_EDITOR_OPTIONS
+  })
 
+  const cssEditor = monaco.editor.create($css, {
+    value: css,
+    language: 'css',
+    ...COMMOND_EDITOR_OPTIONS
+  })
+
+  const jsEditor = monaco.editor.create($js, {
+    value: js,
+    language: 'javascript',
+    ...COMMOND_EDITOR_OPTIONS
+  })
+
+  htmlEditor.onDidChangeModelContent(update)
+  cssEditor.onDidChangeModelContent(update)
+  jsEditor.onDidChangeModelContent(update)
+  
   const htmlForPreview = creatHtml(html, css, js);
   r("iframe").setAttribute("srcdoc", htmlForPreview)
-}
 
 Split({
   columnGutters: [{
@@ -30,18 +67,8 @@ Split({
   }]
 })
 
-const $js = r("#js");
-const $html = r("#html");
-const $css = r("#css");
 
-$js.addEventListener("input", update);
-
-$html.addEventListener("input", update);
-
-$css.addEventListener("input", update);
-
-
-const creatHtml = (html, css, js) => {
+function creatHtml(html, css, js) {
   
 
   return `
@@ -63,9 +90,9 @@ ${js}
 };
 
 function update () {
-  const html = $html.value;
-  const css = $css.value;
-  const js = $js.value;
+  const html = htmlEditor.getValue()
+  const css = cssEditor.getValue()
+  const js = jsEditor.getValue()
   
   const hashedCode = `${encode(html)}|${encode(css)}|${encode(js)}`
   window.history.replaceState(null, null, `/${hashedCode}`)
@@ -73,5 +100,3 @@ function update () {
   const htmlForPreview = creatHtml(html, css, js)
   r("iframe").setAttribute("srcdoc", htmlForPreview)
 };
-
-init()
